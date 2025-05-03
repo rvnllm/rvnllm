5453d2ced15384e6d2fb6be8208f618b700b9334
Ervin Bosenbacher <ervin.bosenbacher@rvnllm.com>
2025-05-03 11:56:45 +0100

    [structure] reorganise crates, remove rvnllm, add parsers

--- parsers/src/tensor.rs
+++ parsers/src/tensor.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 use anyhow::Result;
 
-pub fn load_model<P: AsRef<Path>>(_path: P) -> Result<()>{
+pub fn load_model<P: AsRef<Path>>(path: P) -> Result<()>{
     println!("loading the model");
 
     Ok(())
